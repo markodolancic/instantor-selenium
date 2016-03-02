9566e3710248a67831a350d57fbe7c90d26b4c2d
Marko Dolancic <marko@dolancic.net>
2016-03-02 09:14:27 +0100

    - clean code - update maven build scripts

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Selenium (Instantor)
+
+This is a (changed) fork from [SeleniumHQ](https://github.com/SeleniumHQ/selenium)
--- server/src/main/java/org/openqa/selenium/remote/server/SeleniumServer.java
+++ server/src/main/java/org/openqa/selenium/remote/server/SeleniumServer.java
@@ -18,17 +18,12 @@
 package org.openqa.selenium.remote.server;
 
 import com.beust.jcommander.JCommander;
-
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.openqa.grid.shared.GridNodeServer;
 import org.openqa.selenium.remote.SessionId;
 import org.openqa.selenium.remote.server.handler.DeleteSession;
-import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.HttpConfiguration;
-import org.eclipse.jetty.server.HttpConnectionFactory;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 import javax.servlet.Servlet;
 
@@ -37,169 +32,169 @@ import javax.servlet.Servlet;
  */
 public class SeleniumServer implements GridNodeServer {
 
-  private final int port;
-  private int threadCount;
-  private Server server;
-  private DefaultDriverSessions driverSessions;
+    private final int port;
+    private int threadCount;
+    private Server server;
+    private DefaultDriverSessions driverSessions;
 
-  private Thread shutDownHook;
-  /**
-   * This lock is very important to ensure that SeleniumServer and the underlying Jetty instance
-   * shuts down properly. It ensures that ProxyHandler does not add an SslRelay to the Jetty server
-   * dynamically (needed for SSL proxying) if the server has been shut down or is in the process of
-   * getting shut down.
-   */
-  private final Object shutdownLock = new Object();
-  private static final int MAX_SHUTDOWN_RETRIES = 8;
+    private Thread shutDownHook;
+    /**
+     * This lock is very important to ensure that SeleniumServer and the underlying Jetty instance
+     * shuts down properly. It ensures that ProxyHandler does not add an SslRelay to the Jetty server
+     * dynamically (needed for SSL proxying) if the server has been shut down or is in the process of
+     * getting shut down.
+     */
+    private final Object shutdownLock = new Object();
+    private static final int MAX_SHUTDOWN_RETRIES = 8;
 
 
-  public SeleniumServer(int port) {
-    this.port = port;
-  }
-
-  private void addRcSupport(ServletContextHandler handler) {
-    try {
-      Class<? extends Servlet> rcServlet = Class.forName(
-        "com.thoughtworks.selenium.webdriven.WebDriverBackedSeleniumServlet",
-        false,
-        getClass().getClassLoader())
-        .asSubclass(Servlet.class);
-      handler.addServlet(rcServlet, "/selenium-server/driver/");
-    } catch (ClassNotFoundException e) {
-      // Do nothing.
-    }
-  }
-
-  private void setThreadCount(int threadCount) {
-    this.threadCount = threadCount;
-  }
-
-  public void boot() {
-    if (threadCount > 0) {
-      server = new Server(new QueuedThreadPool(threadCount));
-    } else {
-      server = new Server();
+    public SeleniumServer(int port) {
+        this.port = port;
     }
 
-    ServletContextHandler handler = new ServletContextHandler();
-
-    driverSessions = new DefaultDriverSessions();
-    handler.setAttribute(DriverServlet.SESSIONS_KEY, driverSessions);
-    handler.setContextPath("/");
-    handler.addServlet(DriverServlet.class, "/wd/hub/*");
-    addRcSupport(handler);
-
-    server.setHandler(handler);
-
-    HttpConfiguration httpConfig = new HttpConfiguration();
-    httpConfig.setSecureScheme("https");
-
-    ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(httpConfig));
-    http.setPort(port);
-    http.setIdleTimeout(500000);
-
-    server.setConnectors(new Connector[]{http});
-
-    try {
-      server.start();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private class ShutDownHook implements Runnable {
-    private final SeleniumServer selenium;
-
-    ShutDownHook(SeleniumServer selenium) {
-      this.selenium = selenium;
+    private void addRcSupport(ServletContextHandler handler) {
+        try {
+            Class<? extends Servlet> rcServlet = Class.forName(
+                    "com.thoughtworks.selenium.webdriven.WebDriverBackedSeleniumServlet",
+                    false,
+                    getClass().getClassLoader())
+                    .asSubclass(Servlet.class);
+            handler.addServlet(rcServlet, "/selenium-server/driver/");
+        } catch (ClassNotFoundException e) {
+            // Do nothing.
+        }
     }
 
-    public void run() {
-      selenium.stop();
+    private void setThreadCount(int threadCount) {
+        this.threadCount = threadCount;
     }
-  }
 
-  /**
-   * Stops the Jetty server
-   */
-  public void stop() {
-    int numTries = 0;
-    Exception shutDownException = null;
-
-    // this may be called by a shutdown hook, or it may be called at any time
-    // in case it was called as an ordinary method, try to clean up the shutdown
-    // hook
-    try {
-      if (shutDownHook != null) {
-        Runtime.getRuntime().removeShutdownHook(shutDownHook);
-      }
-    } catch (IllegalStateException ignored) {
-    } // thrown if we're shutting down; that's OK
-
-    // shut down the jetty server (try try again)
-    while (numTries <= MAX_SHUTDOWN_RETRIES) {
-      ++numTries;
-      try {
-        // see docs for the lock object for information on this and why it is IMPORTANT!
-        synchronized (shutdownLock) {
-          server.stop();
+    public void boot() {
+        if (threadCount > 0) {
+            server = new Server(new QueuedThreadPool(threadCount));
+        } else {
+            server = new Server();
         }
 
-        // If we reached here stop didnt throw an exception.
-        // So we assume it was successful.
-        break;
-      } catch (Exception ex) { // org.openqa.jetty.jetty.Server.stop() throws Exception
-        shutDownException = ex;
-        // If Exception is thrown we try to stop the jetty server again
-      }
+        ServletContextHandler handler = new ServletContextHandler();
+
+        driverSessions = new DefaultDriverSessions();
+        handler.setAttribute(DriverServlet.SESSIONS_KEY, driverSessions);
+        handler.setContextPath("/");
+        handler.addServlet(DriverServlet.class, "/wd/hub/*");
+        addRcSupport(handler);
+
+        server.setHandler(handler);
+
+        HttpConfiguration httpConfig = new HttpConfiguration();
+        httpConfig.setSecureScheme("https");
+
+        ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(httpConfig));
+        http.setPort(port);
+        http.setIdleTimeout(500000);
+
+        server.setConnectors(new Connector[]{http});
+
+        try {
+            server.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    // next, stop all of the browser sessions.
-    stopAllBrowsers();
+    private class ShutDownHook implements Runnable {
+        private final SeleniumServer selenium;
 
-    if (numTries > MAX_SHUTDOWN_RETRIES) { // This is bad!! Jetty didnt shutdown..
-      if (null != shutDownException) {
-        throw new RuntimeException(shutDownException);
-      }
-    }
-  }
+        ShutDownHook(SeleniumServer selenium) {
+            this.selenium = selenium;
+        }
 
-  private void stopAllBrowsers() {
-    for (SessionId sessionId : driverSessions.getSessions()) {
-      Session session = driverSessions.get(sessionId);
-      DeleteSession deleteSession = new DeleteSession(session);
-      try {
-        deleteSession.call();
-        driverSessions.deleteSession(sessionId);
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-    }
-  }
-
-  public static void main(String[] argv) {
-    CommandLineArgs args = new CommandLineArgs();
-    JCommander jCommander = new JCommander(args, argv);
-    jCommander.setProgramName("selenium-3-server");
-
-    if (args.help) {
-      StringBuilder message = new StringBuilder();
-      jCommander.usage(message);
-      System.err.println(message.toString());
-      return;
+        public void run() {
+            selenium.stop();
+        }
     }
 
-    SeleniumServer server = new SeleniumServer(args.port);
-    server.setThreadCount(args.jettyThreads);
-    server.boot();
-  }
+    /**
+     * Stops the Jetty server
+     */
+    public void stop() {
+        int numTries = 0;
+        Exception shutDownException = null;
 
-  public static void usage(String msg) {
-    if (msg != null) {
-      System.out.println(msg);
+        // this may be called by a shutdown hook, or it may be called at any time
+        // in case it was called as an ordinary method, try to clean up the shutdown
+        // hook
+        try {
+            if (shutDownHook != null) {
+                Runtime.getRuntime().removeShutdownHook(shutDownHook);
+            }
+        } catch (IllegalStateException ignored) {
+        } // thrown if we're shutting down; that's OK
+
+        // shut down the jetty server (try try again)
+        while (numTries <= MAX_SHUTDOWN_RETRIES) {
+            ++numTries;
+            try {
+                // see docs for the lock object for information on this and why it is IMPORTANT!
+                synchronized (shutdownLock) {
+                    server.stop();
+                }
+
+                // If we reached here stop didnt throw an exception.
+                // So we assume it was successful.
+                break;
+            } catch (Exception ex) { // org.openqa.jetty.jetty.Server.stop() throws Exception
+                shutDownException = ex;
+                // If Exception is thrown we try to stop the jetty server again
+            }
+        }
+
+        // next, stop all of the browser sessions.
+        stopAllBrowsers();
+
+        if (numTries > MAX_SHUTDOWN_RETRIES) { // This is bad!! Jetty didnt shutdown..
+            if (null != shutDownException) {
+                throw new RuntimeException(shutDownException);
+            }
+        }
     }
-    CommandLineArgs args = new CommandLineArgs();
-    JCommander jCommander = new JCommander(args);
-    jCommander.usage();
-  }
+
+    private void stopAllBrowsers() {
+        for (SessionId sessionId : driverSessions.getSessions()) {
+            Session session = driverSessions.get(sessionId);
+            DeleteSession deleteSession = new DeleteSession(session);
+            try {
+                deleteSession.call();
+                driverSessions.deleteSession(sessionId);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void main(String[] argv) {
+        CommandLineArgs args = new CommandLineArgs();
+        JCommander jCommander = new JCommander(args, argv);
+        jCommander.setProgramName("selenium-3-server");
+
+        if (args.help) {
+            StringBuilder message = new StringBuilder();
+            jCommander.usage(message);
+            System.err.println(message.toString());
+            return;
+        }
+
+        SeleniumServer server = new SeleniumServer(args.port);
+        server.setThreadCount(args.jettyThreads);
+        server.boot();
+    }
+
+    public static void usage(String msg) {
+        if (msg != null) {
+            System.out.println(msg);
+        }
+        CommandLineArgs args = new CommandLineArgs();
+        JCommander jCommander = new JCommander(args);
+        jCommander.usage();
+    }
 }
